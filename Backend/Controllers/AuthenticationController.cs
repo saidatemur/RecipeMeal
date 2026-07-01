32d8525c13e6a716e006fae16a70acbe2e2d211b
saidatemur <167565013+saidatemur@users.noreply.github.com>
2026-07-02 01:36:48 +0500

    Update AuthenticationController.cs

--- Backend/Controllers/AuthenticationController.cs
+++ Backend/Controllers/AuthenticationController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
-using Azure.Identity;
 using RecipeApi.Dtos;
 using RecipeApi.Models;
 using RecipeApi.Services;
